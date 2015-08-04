9f0257fb4b41911de1ec354a32e434f4c614d9c5
Mist02468 <mist@ptd.net>
2015-08-04 15:13:36 -0400

    Closer to Working, start like works when used by hand, still working on get with access token

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -9,9 +9,10 @@ class AccessController < ApplicationController
   def startLinkedInAuth
     require 'oauth2'
 
-    client = OAuth2::Client.new('75yetg1f8atx89', 'le39CGDc1yQLCo9U', :site => 'https://www.linkedin.com/uas/oauth2/authorization')
+    client = OAuth2::Client.new('75yetg1f8atx89', 'le39CGDc1yQLCo9U', :site => 'https://www.linkedin.com/', :authorize_url => '/uas/oauth2/authorization')
 
-	redirect_to client.auth_code.authorize_url(:redirect_uri => 'http://localhost:3000/access/finishLinkedInAuth')
+	#redirect_to client.auth_code.authorize_url(:redirect_uri => 'http://localhost:3000/access/finishLinkedInAuth') + '&state=' + getRandomState #might want to apply for '&scope=r_fullprofile' and more
+	puts client.auth_code.authorize_url(:redirect_uri => 'http://localhost:3000/access/finishLinkedInAuth') + '&state=' + getRandomState #might want to apply for '&scope=r_fullprofile' and more
   end
 
   def finishLinkedInAuth
@@ -21,10 +22,11 @@ class AccessController < ApplicationController
     if params[:code].present? && params[:state].present?
       puts "Successful"
 
-      client = OAuth2::Client.new('75yetg1f8atx89', 'le39CGDc1yQLCo9U', :site => 'https://www.linkedin.com/uas/oauth2/accessToken')
-      token = client.auth_code.get_token('authorization_code_value', :redirect_uri => 'http://localhost:3000/access/finishLinkedInAuth', :headers => {'Authorization' => 'Basic some_password'})
+      client = OAuth2::Client.new('75yetg1f8atx89', 'le39CGDc1yQLCo9U', :site => 'https://www.linkedin.com/', :token_url => '/uas/oauth2/accessToken')
+      token = client.auth_code.get_token(params[:code], :redirect_uri => 'http://localhost:3000/access/finishLinkedInAuth')
 
-      #response = token.get('/api/resource', :params => { 'query_foo' => 'bar' })
+      response = token.get('https://api.linkedin.com/v1/people/', :params => { 'format' => 'json' }, :headers => { 'authorization' => 'Bearer ' + token.token })
+      pp response
       #response.class.name
     elsif params[:error].present? && params[:error_description].present?
       puts "Rejected"
@@ -32,4 +34,17 @@ class AccessController < ApplicationController
       puts "Unexpected response"
     end
   end
+  
+  private 
+  
+  def getRandomState
+	prng = Random.new()
+	
+	randStringOfNums = ''
+	for i in 0..20
+		randStringOfNums += prng.rand(0..9).to_s
+	end
+	return randStringOfNums
+  end
+  
 end
