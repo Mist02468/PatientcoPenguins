c6530cbe49a2a0b1234be805f33b80754abe4b16
Mist02468 <mist@ptd.net>
2015-08-07 16:35:12 -0400

    Migration for users

--- db/migrate/20150807175450_create_users.rb
+++ db/migrate/20150807175450_create_users.rb
@@ -1,7 +1,16 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+	  #need to add references to subscriptions (tags or users)
+	  #need to decide exactly which fields we want to get from LinkedIn, these are just for starters
+	  t.string :firstName
+	  t.string :lastName
+	  t.string :location
+	  t.integer :industry
+	  t.integer :numConnections
+	  t.string :position
+	  t.string :company
+	  t.integer :reportedCount
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150807175124) do
+ActiveRecord::Schema.define(version: 20150807175450) do
 
   create_table "posts", force: :cascade do |t|
     t.integer  "type",       default: 0
@@ -23,6 +23,19 @@ ActiveRecord::Schema.define(version: 20150807175124) do
 
   create_table "tags", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "firstName"
+    t.string   "lastName"
+    t.string   "location"
+    t.integer  "industry"
+    t.integer  "numConnections"
+    t.string   "position"
+    t.string   "company"
+    t.integer  "reportedCount"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
 end
