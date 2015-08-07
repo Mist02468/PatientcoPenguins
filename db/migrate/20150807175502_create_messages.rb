06b2324ef5656efb81ef3d753d277f2f603ad544
Mist02468 <mist@ptd.net>
2015-08-07 16:40:43 -0400

    Migration for messages

--- db/migrate/20150807175502_create_messages.rb
+++ db/migrate/20150807175502_create_messages.rb
@@ -1,7 +1,9 @@
 class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
-
+	  #need to add references to users, the sender and receiver
+	  t.string :subject
+	  t.text :text
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150807175450) do
+ActiveRecord::Schema.define(version: 20150807175502) do
+
+  create_table "messages", force: :cascade do |t|
+    t.string   "subject"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.integer  "type",       default: 0
