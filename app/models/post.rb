a1de28c6e45c2fd53afef017f1007ff619c6994f
Mist02468 <mist@ptd.net>
2015-08-07 23:00:42 -0400

    Migration to Add Post Belongs to User Reference

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
 	enum type: [ :share, :question, :discussion, :comment ]
+	belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150807175513) do
+ActiveRecord::Schema.define(version: 20150808025841) do
 
   create_table "events", force: :cascade do |t|
     t.string   "topic"
@@ -33,7 +33,10 @@ ActiveRecord::Schema.define(version: 20150807175513) do
     t.integer  "voteCount",  default: 0
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "tags", force: :cascade do |t|
     t.string "name"
