33f6aabd05602337a9eb0276f3a764315e528088
Mist02468 <mist@ptd.net>
2015-09-22 23:43:16 -0400

    Finishing LinkedIn Profile Sync

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150923011535) do
+ActiveRecord::Schema.define(version: 20150923033942) do
 
   create_table "events", force: :cascade do |t|
     t.string   "topic"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20150923011535) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.string   "linkedInId"
+    t.string   "emailAddress"
   end
 
 end
