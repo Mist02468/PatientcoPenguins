ce38896acd6faa08472b7cf4c4bec6e118766ac7
Mist02468 <mist@ptd.net>
2015-11-04 15:57:41 -0500

    Found Another Bug, With Profiles, They Were Showing the Current User's Posts, Not the Person Whose Profile This Is, Fixed Now

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -14,8 +14,7 @@ class ProfileController < ApplicationController
   def show
 		if params[:user_id].present?
 			@user = User.find(params[:user_id])
+            @posts = Post.where("user_id = #{@user.id}").where("kind = 0").order("voteCount DESC").order("created_at DESC")
 		end
-
-    @posts = Post.all
   end
 end
