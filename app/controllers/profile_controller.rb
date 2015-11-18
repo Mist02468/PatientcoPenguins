83d2ed19c5a3f9a17b8fcd56b095873f91dba5a0
linalana <lin.alanarose@gmail.com>
2015-11-18 00:05:40 -0500

    Added conditionals for reporting. No repeats or self reports.

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -6,7 +6,6 @@ class ProfileController < ApplicationController
 		if params[:reported_user_id].present?
 			@user = User.find(params[:reported_user_id])
       @user.reportedCount += 1
-			puts "reportCount " + @user.reportedCount.to_s
 			@report = UserReport.new
 			if params[:reporting_user_id].present?
 				@user2 = User.find(params[:reporting_user_id])
@@ -19,8 +18,8 @@ class ProfileController < ApplicationController
 				@report.message = params[:reason]
 			end
 			@report.save
-			@user.user_reports << @report
-			@user2.users_reporting << @report
+			@user.users_reporting << @report
+			@user2.user_reports << @report
 			@user.save!
 			@user2.save!
 			puts @report.to_s
@@ -38,8 +37,9 @@ class ProfileController < ApplicationController
 		if params[:user_id].present?
 			@user = User.find(params[:user_id])
             @posts = Post.where("user_id = #{@user.id}").where("kind = 0").order("voteCount DESC").order("created_at DESC")
-						@reportable = UserReport.where("reporter_id != #{session[:user_id]}").count == 0
-						puts "reportable is: " + @reportable.to_s
+						@is_user = @user.id == session[:user_id]
+						@reportable = UserReport.where("reported_id == #{@user.id}").where("reporter_id == #{session[:user_id]}").count == 0
+						puts "reportable is " + @reportable.to_s
 		end
   end
 end
