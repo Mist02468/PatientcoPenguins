1b049127fb070add111d807aeb967f2796bf9698
linalana <lin.alanarose@gmail.com>
2015-11-17 19:02:08 -0500

    Tried to fix saving of report to db

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -6,19 +6,23 @@ class ProfileController < ApplicationController
 		if params[:user_id].present?
 			@user = User.find(params[:user_id])
       @user.reportedCount += 1
-			@user.save!
 			puts "reportCount " + @user.reportedCount.to_s
 			@report = UserReport.new
 			if params[:reporting_user_id].present?
+				@user2 = User.find(params[:reporting_user_id])
 				@report.reporter_id = params[:reporting_user_id]
 			end
 			if params[:reported_user_id].present?
-				@report.reporter_id = params[:reporting_user_id]
+				@report.reported_id = params[:reported_user_id]
 			end
 			if params[:reason].present?
 				@report.message = params[:reason]
 			end
-			@report.save!
+			@report.save
+			@user.user_reports << @report
+			@user2.user_reporting << @report
+			@user.save!
+			@user2.save!
 			puts @report.to_s
 			redirect_to(controller: "profile", :action => "show", :user_id => params[:user_id])
 		end
@@ -34,6 +38,8 @@ class ProfileController < ApplicationController
 		if params[:user_id].present?
 			@user = User.find(params[:user_id])
             @posts = Post.where("user_id = #{@user.id}").where("kind = 0").order("voteCount DESC").order("created_at DESC")
+						@reportable = UserReport.where("reporter_id != #{session[:user_id]}").count == 0
+						puts "reportable is: " + @reportable.to_s
 		end
   end
 end
