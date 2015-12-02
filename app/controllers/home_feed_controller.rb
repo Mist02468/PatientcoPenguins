cc6757680de4009c25d98e37644a8aa06f55dcf9
Mike Avery <mikeavery333@gmail.com>
2015-12-01 23:43:00 -0500

    Added second row for events
    Clickable and added the user

--- app/controllers/home_feed_controller.rb
+++ app/controllers/home_feed_controller.rb
@@ -6,9 +6,11 @@ class HomeFeedController < ApplicationController
     if params[:tag_id].present?
         @tag   = Tag.find(params[:tag_id])
         @posts = Post.joins(:tags).where(tags: { id: params[:tag_id] })
+        @events = Event.joins(:tags).where(tags: { id: params[:tag_id] })
     else
         @tag   = nil
         @posts = Post.all
+        @events = Event.all
     end
   end
   
